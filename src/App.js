721c73ac3be730227c9255bf93af9feda41649a4
Shubham Chauhan <shubhamchauhan0304@gmail.com>
2020-06-29 13:14:02 +0530

    Components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Navbar } from 'reactstrap';
 import './App.css';
 import { NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color = "primary">
-        <div className="container">
-          <NavbarBrand href = "/">Ristronte Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
-}
+import {DISHES} from './shared/dishes'
+import { render } from '@testing-library/react';
 
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+    render() {
+      return (
+        <div className="App">
+          <Navbar dark color = "primary">
+            <div className="container">
+              <NavbarBrand href = "/">Ristronte Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes}/>
+        </div>
+      );
+    }
+  }
+  
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,56 +1,23 @@
 // const { Component } = require("react");
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Row, Col
+} from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            dishes: [
-                {
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price: '1.99',
-                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price: '1.99',
-                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price: '2.99',
-                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ],
-        };
+
+        }
     }
 
     render() {
 
-        const menu = this.state.dishes.map((dish) => {
+        const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 mt-5">
                     <Media tag="li">
@@ -63,6 +30,21 @@ class Menu extends Component {
                         </Media>
                     </Media>
                 </div>
+            //     <div key={dish.id} className="col-5 mt-5">
+            //         <Row>
+            //             <Card><Col sm-6>
+            //             <CardImg width="100%" height="50%" src={dish.image} alt="Card image cap" />
+            //             <CardBody>
+            //                 <CardTitle>{dish.name}</CardTitle>
+            //                 <CardSubtitle>Card subtitle</CardSubtitle>
+            //                 <CardText>{dish.description}</CardText>
+            //                 <Button>Click Here</Button>
+            //             </CardBody>
+            //             </Col>
+            //             </Card>
+                        
+            //         </Row>
+            //     </div>
             )
         })
         return (
